074c1e888838741e10098010dbb174653e4c941a
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2023-02-28 20:45:57 +0200

    Add project files.

---
+++ matrixScreen/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace matrixScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tempus Sans ITC", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1272, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 22);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Stencil", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1272, 51);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1272, 78);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 22);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tempus Sans ITC", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(665, 287);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 78);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1698, 680);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Sitka Text", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ matrixScreen/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matrixScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public const int a = 50, aa = 50;
+        public int[,] matricea = new int[a, aa];
+        public Graphics g;
+        public Brush b = new SolidBrush(Color.Green);
+        public Pen p = new Pen(Color.Black);
+        public Color color = Color.White;
+        public Font f;
+        public Random random= new Random();
+        public int sizeoffont = 20;
+        public void drawAllChars()
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < aa; j++)
+                {
+                    matricea[i, j] = random.Next(1,49);
+                    g.DrawString(matricea[i, j].ToString(),f,b,new Point(j* sizeoffont, i * sizeoffont));
+                }
+            }
+        }
+        public void clearAllLines()
+        {
+            //aici puteti sterge doar o linie de sus sau de jos 
+            g.Clear(this.BackColor);
+        }
+        public void animateAllChars()
+        {
+            int k = a;
+            while (k>1) {
+               
+                for (int i = 0; i < a-k; i++)
+                {
+                    for (int j = 0; j < aa; j++)
+                    {
+
+                        //g.DrawString(matricea[random.Next(0, a), j].ToString(), f, b, new Point(i * sizeoffont, j * sizeoffont));
+                        g.DrawString(matricea[j, random.Next(0, a)].ToString(), f, b, new Point(j * sizeoffont, i * sizeoffont));
+                    }
+                }
+                k--;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 1920;
+            this.Height = 1080;
+            this.Left = 0;
+            this.Top = 0;
+            this.BackColor= Color.Black;
+
+            g = CreateGraphics();
+            f = new Font("Wingdings 3", 10);
+           
+        }
+        public void doit()
+        {
+            drawAllChars();
+            clearAllLines();
+            animateAllChars();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawAllChars();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clearAllLines();
+            animateAllChars();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            clearAllLines();
+            animateAllChars();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1;
+            timer1.Enabled = true;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Visible= false;
+            button2.Visible= false;
+            button3.Visible= false;
+            button4.Visible= false;
+            doit();
+            timer1.Interval = 1;
+            timer1.Enabled = true;
+        }
+    }
+}
